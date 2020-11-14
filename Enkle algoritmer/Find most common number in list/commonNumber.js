8f0325c3edd039f87b788a91d9b43b9f3681c505
nathsten <nathsten03@gmail.com>
2020-11-14 20:11:58 +0100

    Finds most common number in array

--- Enkle algoritmer/Find most common number in list/commonNumber.js
+++ Enkle algoritmer/Find most common number in list/commonNumber.js
@@ -1,1 +1,48 @@
 //@ts-check
+
+let numbers = [5,2,4,6,5,7,2,1,2];
+
+/**
+ * @param {Number[]} n
+ * @returns {Number} 
+ */
+const commonNumbers = (n) => {
+    let numberAndIndex = [];
+    let output;
+    let numberTimesFound = [];
+    
+
+    for(let i=0; i<n.length; i++){
+        let timesFound = 1;
+
+        for(let j=0; j<n.length; j++){
+            if(n[i] === n[j] && i !== j){
+                timesFound += 1;
+                numberAndIndex.push([n[j], i, j]);
+            }
+        }
+
+        if(timesFound > 1){
+            if(numberTimesFound.indexOf([n[i], timesFound]) === -1){
+                numberTimesFound.push([n[i], timesFound]);
+            }
+        }
+    }
+    
+    const findCommonNumber = (n) => {
+        let commonNumber = n[0][1];
+
+        for(let i=0; i<n.length; i++){
+            if(n[i][1] > commonNumber){
+                commonNumber = n[i][0];
+            }
+        }
+        return commonNumber;
+    }
+
+    output = findCommonNumber(numberTimesFound);
+
+    return output;
+}
+
+console.log(commonNumbers(numbers));
---
+++ Enkle algoritmer/Find most common number in list/commonNumbers.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public static void main(String args[]){
+        int[] tall1 = {5,2,4,6,7,2,1,2};
+        System.out.println(commonNumbers(tall1));
+    }
+
+    public static float commonNumbers(int[] nums) {
+        float output;
+        output = nums[1];
+        return output;
+    }
+
+}
