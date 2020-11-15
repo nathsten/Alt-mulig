64e742ad60abdf7b9fe719fbccd6a8046f9f54e9
nathsten <nathsten03@gmail.com>
2020-11-15 19:52:54 +0100

    Finner typetall med Java

--- Enkle algoritmer/Find most common number in list/commonNumbers.java
+++ Enkle algoritmer/Find most common number in list/commonNumbers.java
@@ -1,14 +1,39 @@
-class Solution {
+class commonNumbers {
 
     public static void main(String args[]){
-        int[] tall1 = {5,2,4,6,7,2,1,2};
-        System.out.println(commonNumbers(tall1));
+        int[] tall = {5,2,4,6,7,2,1,2};
+        System.out.println(sortNums((findCommonNumber(tall))));
     }
 
-    public static float commonNumbers(int[] nums) {
-        float output;
-        output = nums[1];
-        return output;
+    public static int[][] findCommonNumber(int[] nums) {
+        // float output;
+        int[][] numberTimesFound = {};
+
+        for(int i=0; i<nums.length; i++){
+            int timesFound = 1;
+            
+            for(int j=0; j<nums.length; j++){
+                if(nums[i] == nums[j] && i != j){
+                    timesFound += 1;
+                }
+            }
+            if(timesFound > 1){
+                int[] CN = {nums[i], timesFound};
+                int poss = numberTimesFound.length;
+                numberTimesFound[poss] = CN;
+            }
+        }
+        return numberTimesFound;
     }
 
+    public static int sortNums(int[][] n) {
+        int commonNumber = n[0][0];
+
+        for(int i=0; i<n.length; i++){
+            if(n[i][0] > commonNumber){
+                commonNumber = n[i][0];
+            }
+        }
+        return commonNumber;
+    }
 }
