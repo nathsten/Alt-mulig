ddc6d432b9027ba00562de6c34c4489e80918c44
nathsten <nathsten03@gmail.com>
2020-11-30 08:50:26 +0100

    score

--- Block jump/block.js
+++ Block jump/block.js
@@ -2,14 +2,23 @@ const $ = (id) => document.getElementById(id);
 
 const player = $("player");
 const gameDiv = $("gameDiv");
+const scoreDiv = $("score");
+const highscoreDiv = $("highscore");
 
 let allIntervals = [];
 
 let jumping = false;
+let playing = false;
+let points = 1;
 
 let playerY = 0;
 let playerx = 40;
 let playerVY = 1;
+
+let score = 0;
+let scoreArr = [];
+
+let scoreInter;
 
 const jump = () => {
     playerY = 1;
@@ -81,29 +90,49 @@ const moveBlock = () => {
     allIntervals.push(moveBlock, vankslighetsgrad);
 }
 
-const checkSpace = (e) => {if(e.keyCode === 32){
-    if(jumping !== true){
+const checkSpace = (e) => {if(e.keyCode === 32 || e.keyCode === 13){
+    if(jumping !== true && playing !== false){
         jump();
         jumping = true;
     }
 }};
 
-moveBlock();
-
-const checkFail = () => {
+const checkEnter = (e) => {if(e.keyCode === 32){
+    if(playing !== true){
+        if($("block")){
+            $("block").remove();
+        }
+        moveBlock();
+        jump();
+        scoreUp();
+        playing = true;
+    }
+}};
+function checkFail(){
     if((blockX > 460 && blockX < 540) && (playerY < 40)){
-        alert("Feil");
         allIntervals.forEach((e) => clearInterval(e));
+        alert("Fail");
         blockX = -40;
         blockVX = 2;
         playerY = 0;
+        score = 0;
         $("block").style.right = `${blockX}px`;
         player.style.bottom = `${playerY}px`;
+        playing = false;
     }
 }
 
 setInterval(() => {
     checkFail();
-}, 10);
+}, 1);
+
+function scoreUp(){
+    scoreInter = setInterval(() => {
+        score += points;
+        scoreDiv.innerHTML = `Score: ${score}`;
+    }, 100);
+    allIntervals.push(scoreInter);
+}
 
 document.addEventListener("keydown", checkSpace);
+document.addEventListener("keydown", checkEnter);
