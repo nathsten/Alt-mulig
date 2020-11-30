6088101eccbb69d3edf3cd4cb997314ef0490d91
nathsten <nathsten03@gmail.com>
2020-11-30 17:15:25 +0100

    Fikset MAASSE bugs

--- Block jump/block.js
+++ Block jump/block.js
@@ -7,46 +7,131 @@
 const $ = (id) => document.getElementById(id);
 
 let player = $("player");
+let block = $("block");
 const gameDiv = $("gameDiv");
 const scoreDiv = $("score");
 const highscoreDiv = $("highscore");
 
-// Vet ikke om dette blir rett, må finne ut hvordan jeg skal flytte flere av intervallene
-//  og variablene inn i class.
-class Player{
-    x = 0;
-    y = 0;
-    vy = 1;
-    ay = 0.2;
-    div = player;
-
-    jump(){
-        this.div.style.left = `${this.x}px`;
-        // Skirve om hele jump funksjonen her...
-    }
-}
-
-player = new Player;
-player.x = 80;
-player.y = 0;
-player.vy = 1;
-player.ay = 0.2;
-player.jump();
-
-let allIntervals = [0];
-
+let playerMove;
+let playerAX;
+let blockMove;
+let blockAX;
+let blockAXTime = 100;
+let blockAXIntervall;
 let jumping = false;
 let playing = false;
 let points = 1;
-
-let playerY = 0;
-let playerx = 40;
-let playerVY = 1;
-
+let allIntervals = [];
 let score = 0;
 let scoreArr = [0];
+class gameObject{
+    x = 0;
+    y = 0;
+    vy = 1;
+    vx = 1;
+    ay = 0.2;
+    ax = 0.2;
+    div = undefined;
 
-let scoreInter;
+    jump(){
+        this.y = 0;
+        this.ay = 0.2;
+        this.vy = 1;
+        playerAX = setInterval(() => {
+            this.vy += this.ay;
+        }, 1);
+        playerMove = setInterval(() => {
+            this.y += this.vy;
+            this.div.style.bottom = `${this.y}px`;
+            if(this.y > 80){
+                this.ay = -0.2;
+            }
+            if(this.y < 0){
+                clearInterval(playerAX);
+                clearInterval(playerMove);
+                jumping = false;
+            }
+        }, 10);
+    allIntervals.push(playerAX, playerMove);
+    }
+
+    moveBlock(){
+        blockAXIntervall = setInterval(() => {
+            blockAXTime -= 5;
+        }, 1000);
+
+        blockAX = setInterval(() => {
+            this.vx += this.ax;
+            score += points;
+            scoreDiv.innerHTML = `Score: ${score}`;
+        }, blockAXTime);
+
+        blockMove = setInterval(() => {
+            this.x += this.vx;
+            this.div.style.right = `${this.x}px`;
+            checkFail(player.y, block.x);
+            if(this.x > 600){
+                this.x = -40;
+            }
+        }, 10);
+    allIntervals.push(blockAX, blockMove, blockAXIntervall);
+    }
+}
+
+player = new gameObject;
+player.x = 60;
+player.y = 0;
+player.vy = 1;
+player.ay = 0.2;
+player.div = $("player");
+
+block = new gameObject;
+block.x = -40;
+block.y = 0;
+block.vx = 2;
+block.ax = 0.01;
+block.div = $("block");
+
+/** Starter spill
+ * @param {{ keyCode: number; key: string; }} e
+ */
+const startSpill = (e) => { if(e.keyCode === 32 || e.key === 'Enter' || e.keyCode === 38){
+        if(playing !== true){
+            block.moveBlock();
+            player.jump();
+            jumping = true;
+            playing = true;
+        }
+    }
+}
+
+/** Hopper hver gang du trykker space
+ * @param {{ keyCode: number; key: string; }} e
+ */
+const checkSpace = (e) => {if(e.keyCode === 32 || e.keyCode === 38){
+    if(jumping !== true && playing !== false){
+        player.jump();
+        jumping = true;
+    }
+}};
+
+/** Sjekker om player er borti block
+ * @param {number} p
+ * @param {number} b
+ */
+const checkFail = (p, b) => {
+    if(p < 40 && b > 480 && b < 540){
+        allIntervals.forEach((e) => clearInterval(e));
+        jumping = false;
+        playing = false;
+        scoreArr.push(score);
+        highscore(scoreArr);
+        score = 0;
+        block.x = -40;
+        blockAXTime = 100;
+        block.vx = 2;
+    }
+}
 
 /**
  * @param {Number[]} arr
@@ -57,135 +142,10 @@ function highscore(arr){
         if(arr[i] > hs){hs = arr[i]};
     }
     highscoreDiv.innerHTML = `Highscore: ${hs}`;
+    scoreDiv.innerHTML = `Score: ${score}`;
 }
 
 highscore(scoreArr);
 
-const jump = () => {
-    playerY = 0;
-    playerVY = 1;
-    let move;
-    let vxup;
-    let down;
-
-    vxup = setInterval(() => {
-        playerVY += 0.2;
-    }, 1);
-
-    move = setInterval(() => {
-        playerY += playerVY;
-        player.style.bottom = `${playerY}px`;
-    }, 10);
-
-    setTimeout(() => {
-        clearInterval(vxup)
-        down = setInterval(() => {
-            playerVY -= 0.15;
-            if(playerY < 0){
-                clearInterval(move);
-                clearInterval(down);
-                playerY = 1;
-                jumping = false;
-            }
-        }, 1);
-    }, 130);
-    allIntervals.push(move, vxup, down);
-}
-
-let blockX = -40;
-let blockVX = 2;
-
-const moveBlock = () => {
-    const block = document.createElement("div");
-    block.id = "block";
-    gameDiv.append(block);
-
-    let moveBlock;
-    let vankslighetsgrad;
-    let time = 10;
-
-    blockVX = 2;
-    blockX = -40
-
-    function createMoveBlockTimer(){
-        moveBlock = setInterval(() => {
-            blockX += blockVX;
-            block.style.right = `${blockX}px`;
-            checkX();
-        }, time);
-    }
-
-    createMoveBlockTimer();
-
-    vankslighetsgrad = setInterval(() => {
-        blockVX += 0.5
-        if(time > 0){
-            time -=1;
-        }
-    }, 5000);
-
-    function checkX(){
-        if(blockX > 600){
-            clearInterval(moveBlock);
-            blockX = -40;
-            createMoveBlockTimer();
-        }
-    }
-    allIntervals.push(moveBlock, vankslighetsgrad);
-}
-
-/**
- * @param {{ keyCode: number; key: string; }} e
- */
-const checkSpace = (e) => {if(e.keyCode === 32){
-    if(jumping !== true && playing !== false){
-        jump();
-        jumping = true;
-    }
-}};
-
-/**
- * @param {{ keyCode: number; key: string; }} e
- */
-const checkEnter = (e) => {if(e.keyCode === 32 || e.key === 'Enter'){
-    if(playing !== true){
-        if($("block")){
-            $("block").remove();
-        }
-
-        moveBlock();
-        jump();
-        scoreUp();
-        playing = true;
-    }
-}};
-function checkFail(){
-    if((blockX > 460 && blockX < 540) && (playerY < 40)){
-        allIntervals.forEach((e) => clearInterval(e));
-        alert("Fail");
-        blockX = -40;
-        blockVX = 2;
-        playerY = 0;
-        scoreArr.push(score);
-        highscore(scoreArr);
-        score = 0;
-        $("block").style.right = `${blockX}px`;
-        player.style.bottom = `${playerY}px`;
-        playing = false;
-    }
-}
-
-setInterval(() => {
-    checkFail();
-}, 1);
-
-function scoreUp(){
-    scoreInter = setInterval(() => {
-        score += points;
-        scoreDiv.innerHTML = `Score: ${score}`;
-    }, 100);
-    allIntervals.push(scoreInter);
-}
-
 document.addEventListener("keydown", checkSpace);
-document.addEventListener("keydown", checkEnter);
+document.addEventListener("keydown", startSpill);
