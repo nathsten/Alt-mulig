2ddd752f8e1f6d8f71694e6873b41dd41a8add6f
nathsten <nathsten03@gmail.com>
2020-12-04 15:02:57 +0100

    Kjapp fiks på arduino

--- Arduino Controller/index.js
+++ Arduino Controller/index.js
@@ -1,14 +1,17 @@
 const {Board, Led} = require("johnny-five");
 const express = require('express');
+const fs = require('fs');
 const port = 3000;
 const index = express();
 const server = index.listen(port, callBack);
 
-index.use(express.static('public'))
+index.use(express.static('public'));
+
+const getBoardState = JSON.parse(fs.readFileSync('state.json'));
 
 function callBack(error){
     if(error){
-        console.error('Noe gikk galt...')
+        console.error('Noe gikk galt...');
     }
     else{
         console.log(`Server kjører på localhost:${port}`);
@@ -22,10 +25,18 @@ function turnOnFunk(req, res){
     if(onOff === 'on'){
         console.log('Turning on...');
         turnOn();
+        getBoardState["lightOn"] = true;
+        setTimeout(() => {
+            fs.writeFileSync('state.json', JSON.stringify(getBoardState, null, 2));            
+        }, 2500);
     }
     else if(onOff === 'off'){
         console.log('Turning off...')
         turnOff();
+        getBoardState["lightOn"] = false;
+        setTimeout(() => {
+            fs.writeFileSync('state.json', JSON.stringify(getBoardState, null, 2));            
+        }, 2500);
     }
     else{
         console.log('Fokk...');
