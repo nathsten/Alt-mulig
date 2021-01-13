638aca1b45c23e255063fa46420f1399d1430c6f
nathsten <nathsten03@gmail.com>
2021-01-13 17:12:04 +0100

    Connected calendar app to pg database

---
+++ Calendar app/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { client } = require('./client.js');
+const { encryptPassword } = require('./passProtect.js');
+const { decryptPasword } = require('./passProtect.js');
+const port = 8080;
+const index = express();
+index.use(cookieParser());
+index.use(bodyParser.urlencoded({extended: true}));
+
+index.listen(port, (err) => {
+    if(!err){
+        console.log(`Listening on localhost:${port}`)
+    }
+    else{
+        console.error(`Something went wrong: ${err}`);
+    }
+})
+
+client.connect();
+
+index.get('/', async (req, res) => {
+    const queryUserlist = await client.query(`SELECT * FROM userlist;`);
+    const data = await queryUserlist.rows;
+    res.json(data);
+});
