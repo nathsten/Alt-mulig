0eaffcf4e95d7fe8dc0afa177821e21b6f98986a
nathsten <nathsten03@gmail.com>
2021-04-28 10:58:50 +0200

    fix før hosting

--- Exam db/app.js
+++ Exam db/app.js
@@ -19,7 +19,7 @@ app.listen(PORT, e => e ? console.log(e) : console.log(`Listening on port:${PORT
 
 // Henter alle eksamensett og oppdaterer databasen. 
 app.post('/oppdaterEksamensoppgaver', async (req, res) => {
-    const alleFag = "S1 S2 R1 R2 1P 2P".split(" ");
+    const alleFag = "S1 S2 R1 R2 1P 2P 1T".split(" ");
     var { month, year } = req.body;
     month = +month;
     year = +year;
@@ -36,7 +36,11 @@ app.post('/oppdaterEksamensoppgaver', async (req, res) => {
             return;
         }
     } catch(e){ /** Må oppdateres */ }
-    
+
+    // noterer oppdatering
+    updates.updates.push({month, year, updated: true});
+    writeFileSync('update.json', JSON.stringify(updates, null, 2));
+
     const browser = await ws.launch();
     for(const fag of alleFag) {
         console.log(fag);
@@ -44,7 +48,8 @@ app.post('/oppdaterEksamensoppgaver', async (req, res) => {
         // går til valgt matte sin hovedside
         await page.goto(`https://matematikk.net/side/${fag}_Hovedside`);
         // liste over alle eksamenene som er gitt
-        const [el] = await page.$x("/html/body/div[5]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl");
+        const xpath = fag !== "1T" ? "/html/body/div[5]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl" : "/html/body/div[6]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl"
+        const [el] = await page.$x(xpath);
         const getText = await el.getProperty("textContent");
         const text = await getText.jsonValue();
         // antall eksamensett som finnes
@@ -57,11 +62,18 @@ app.post('/oppdaterEksamensoppgaver', async (req, res) => {
         const alleEksamenForFag = {}
         let fagnr = 0;
         for(let i = 1; i < antallOppg*2; i+=2){
-            const [elLink] = await page.$x(`/html/body/div[5]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl/dd[${i}]/a`);
+            const år = await eksamenFag[fagnr];
+            if(fag === "1T" && år === "Høst 2020") {
+                fagnr += 5;
+                continue;
+            }
+            if(fag === "1T" && år === "Høst 2010") break;
+            console.log(`Fag: ${fag}, år: ${år}`);
+            const x = fag !== "1T" ? `/html/body/div[5]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl/dd[${i}]/a` : `/html/body/div[6]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl/dd[${i}]/a`
+            const [elLink] = await page.$x(x);
             const getLink = await elLink.getProperty("href");
             const link = await getLink.jsonValue();
             const readPdf = await getPdfObject(link);
-            const år = await eksamenFag[fagnr];
             fagnr += 3;
             alleEksamenForFag[år] = await {link, readPdf};
         }
@@ -70,11 +82,50 @@ app.post('/oppdaterEksamensoppgaver', async (req, res) => {
     browser.close();
     // skriver til databasen
     writeFileSync('db.json', JSON.stringify(db, null, 2));
-    updates.updates.push({month, year, updated: true});
-    // noterer oppdatering
-    writeFileSync('update.json', JSON.stringify(updates, null, 2));
     res.send({status: "Database oppdatert"})
+    console.log("Database oppdatert");
 })
+
+// debugging
+app.get('/oppdaterEksamensoppgaver/:fag', async (req, res) => {
+    const fag = req.params.fag;
+    const browser = await ws.launch();
+    const page = await browser.newPage();
+    // går til valgt matte sin hovedside
+    await page.goto(`https://matematikk.net/side/${fag}_Hovedside`);
+    // liste over alle eksamenene som er gitt
+    const xpath = fag !== "1T" ? "/html/body/div[5]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl" : "/html/body/div[6]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl"
+    const [el] = await page.$x(xpath);
+    const getText = await el.getProperty("textContent");
+    const text = await getText.jsonValue();
+    // antall eksamensett som finnes
+    const antallOppg = await Math.floor(text.split(/\n/).length/3);
+    // Navn på alle eksamenssett. 
+    const eksamenFag = await text.split(/\n/);
+    const alleEksamenForFag = {}
+    let fagnr = 0;
+    for(let i = 1; i < antallOppg*2; i+=2){
+        const år = await eksamenFag[fagnr];
+        if(år === "Høst 2020") {
+            fagnr += 5;
+            continue;
+        }
+        if(år === "Høst 2010") break;
+        console.log(år);
+        const x = fag !== "1T" ? `/html/body/div[5]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl/dd[${i}]/a` : `/html/body/div[6]/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/dl/dd[${i}]/a`
+        const [elLink] = await page.$x(x);                   
+        const getLink = await elLink.getProperty("href");
+        const link = await getLink.jsonValue();
+        const readPdf = await getPdfObject(link);
+        fagnr += 3;
+        alleEksamenForFag[år] = await {link, readPdf};
+    }
+    db[fag] = alleEksamenForFag;
+    writeFileSync('tdb.json', JSON.stringify(db, null, 2));
+    res.send(alleEksamenForFag);
+    browser.close();
+})
+
 
 app.get('/finnEksamensOppgaver/:fag/:sokeord', (req, res) => {
     const fag = req.params.fag;
