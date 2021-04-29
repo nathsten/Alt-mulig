cacd98a8a64e859ae5b6ff32e61157a7b6934451
nathsten <nathsten03@gmail.com>
2021-04-29 21:32:26 +0200

    eks

--- IT2 2021/Uke 17/Oppg3 V2018/oppg3.js
+++ IT2 2021/Uke 17/Oppg3 V2018/oppg3.js
@@ -47,17 +47,59 @@ const hentLyder = async lydNavn => new Promise((resolve, reject) => {
     }
 })
 
-const setup = async () => {
-    const [ spilleListe ] = $("spilleListe");
-    const lydNavn = "fagott.floyte.klarinett.obo.valthorn.feil.riktig".split(".");
-    const bildeNavn = lydNavn.slice(0, lydNavn.length-2);
-    const lydListe = await hentLyder(lydNavn);
-    const bildeListe = await hentBilder(bildeNavn);
-    await bildeListe.forEach(async bilde => {
+/**
+ * @param {Promise<{[string]: string;}>[]} liste med bilder
+ * @param {HTMLElement} divB
+ */
+const printBilder = (bildeListe, divB) => {
+    divB.innerHTML = "";
+    bildeListe.forEach(async (bilde, i) => {
         const div = new$("div");
         div.style.backgroundImage = `url(${await bilde})`;
         div.className = "bilde";
-        spilleListe.append(div);
+        div.id = i;
+        divB.append(div);
+    })
+}
+
+const velgInstrument = l => Math.floor(Math.random()*l.length);
+
+const setup = async () => {
+    const [ spilleListe, start, spillAvIgjen, p ] = $("spilleListe start sa p");
+    const lydNavn = "fagott.floyte.klarinett.obo.valthorn.feil.riktig".split(".");
+    const bildeNavn = lydNavn.slice(0, lydNavn.length-2);
+    var lydListe = await hentLyder(lydNavn);
+    var bildeListe = await hentBilder(bildeNavn);
+    const instrumentListe = await lydListe.map(e => Object.keys(e).join("")).slice(0, 5);
+    printBilder(bildeListe, spilleListe);
+    var poeng = 0;
+    const sai = (i) => lydListe[i][instrumentListe[i]].play()
+    
+    // idk funka dårlig. 
+    start.addEventListener("click", () => {
+        start.style.display = "none";
+        const spill = () => {
+            const i = velgInstrument(bildeListe);
+            lydListe[i][instrumentListe[i]].play();
+            const check = e => {
+                const { id } = e.target;
+                if(+id === i){
+                    poeng += 100;
+                    lydListe[i][instrumentListe[i]].pause();
+                    bildeListe = bildeListe.filter((e, ix) => ix !== i);
+                    lydListe = lydListe.filter((e, ix) => ix !== i);
+                    printBilder(bildeListe, spilleListe);
+                    spillAvIgjen.removeEventListener("click", sai);
+                    spilleListe.removeEventListener("click", check);
+                    spill();
+                }
+                else poeng -= 50;
+                p.innerHTML = `Poeng: ${poeng}`;
+            }
+            spillAvIgjen.addEventListener("click", () => sai(i));
+            spilleListe.addEventListener("click", check)
+        }
+        spill();
     })
 }
 
