880854adb3df13c18a3db5507c9a67037968a25e
nathsten <nathsten03@gmail.com>
2021-06-16 11:36:24 +0200

    actix api with postgreSQL

--- Rust/actix_api/src/main.rs
+++ Rust/actix_api/src/main.rs
@@ -1,7 +1,13 @@
-use actix_web::{get, web, App, Error, HttpServer, HttpRequest, HttpResponse, Responder, Result};
+use actix_web::{get, web, App, HttpServer, HttpRequest, HttpResponse, Responder, Result};
 use actix_web::http::{StatusCode};
-use actix_web::http::header::{ContentDisposition, DispositionType};
-use actix_files as fs;
+use tokio_postgres::NoTls;
+use dotenv::dotenv;
+use deadpool_postgres;
+use deadpool_postgres::{Client};
+use serde::{Deserialize, Serialize};
+use config::{ConfigError};
+use tokio_pg_mapper::{FromTokioPostgresRow};
+use tokio_pg_mapper_derive::PostgresMapper;
 
 #[get("/")]
 async fn home() -> Result<HttpResponse> {
@@ -15,19 +21,6 @@ async fn new_user(web::Path((username, pwd)): web::Path<(String, String)>) -> im
     format!("username: {}, password: {}", username, pwd)
 }
 
-// fix this
-#[get("/{filename:.*}")]
-async fn index(req: HttpRequest) -> Result<fs::NamedFile, Error> {
-    let path: std::path::PathBuf = req.match_info().query("../public").parse().unwrap();
-    let file = fs::NamedFile::open(path)?;
-    Ok(file
-        .use_last_modified(true)
-        .set_content_disposition(ContentDisposition {
-            disposition: DispositionType::Attachment,
-            parameters: vec![],
-        }))
-}
-
 async fn with_param(req: HttpRequest, web::Path((name,)): web::Path<(String,)>,) -> HttpResponse {
     println!("{:?}", req);
 
@@ -36,16 +29,65 @@ async fn with_param(req: HttpRequest, web::Path((name,)): web::Path<(String,)>,)
         .body(format!("Hello {}!", name))
 }
 
+#[derive(Deserialize)]
+struct ServerConfig{
+    pub host: String,
+    pub port: i32
+}
+
+#[derive(Deserialize)]
+struct Config{
+    pub server: ServerConfig,
+    pub pg: deadpool_postgres::Config 
+}
+
+impl Config{
+    pub fn from_env() -> Result<Self, ConfigError>{
+        let mut cfg = config::Config::new();
+        cfg.merge(config::Environment::new())?;
+        cfg.try_into()
+    }
+}
+
+#[derive(Serialize, serde::Deserialize, PostgresMapper)]
+#[pg_mapper(table = "todos")]
+struct Todo{
+    pub id: i32,
+    pub name: String,
+    pub checked: bool
+}
+
+/**
+  thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: missing field `pg`', src/main.rs:46:35
+  note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+  Panic in Arbiter thread.
+*/
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-        .service(index)
+
+    
+    dotenv().ok();
+
+    let cnfg = Config::from_env().unwrap();
+
+    let pool = cnfg.pg.create_pool(NoTls).unwrap();
+
+    let client: Client = pool.get().await.expect("No db");
+
+    let state = client.prepare("SELECT * FROM todos;").await.unwrap();
+    let todos = client.query(&state, &[]).await.expect("No todos");
+    // .collect::<Vec<Todo>>();
+    println!("{:?}", todos);
+
+    HttpServer::new(||App::new()
         .service(home)
         .service(web::resource("user/{name}").route(web::get().to(with_param)))
         // static folder.
         // .service(fs::Files::new("../public", "static").show_files_listing())
         )
-        .bind("127.0.0.1:8080")?
+        .bind(format!("{}:{}", cnfg.server.host, cnfg.server.port))?
         .run()
         .await
 }
